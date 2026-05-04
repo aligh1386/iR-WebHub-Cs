3d764dd563f0673b3869bb9601ef1d4d75f7b715
aligh1386 <aligh13861386@gmail.com>
2026-05-04 13:41:14 +0330

    Add files via upload

---
+++ iRWebHub/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("A program for Iran’s National Internet")]
+[assembly: AssemblyDescription("Ali gholampour")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ali gholampour")]
+[assembly: AssemblyProduct("iRWebHub")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("872d338e-675c-422a-b5d3-975246ad94a6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.3.8.6")]
+[assembly: AssemblyFileVersion("1.3.8.6")]
